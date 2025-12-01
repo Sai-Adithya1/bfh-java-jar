a851a80747aedbb92d8a493e08fb0d69929f3574
Sai-Adithya1 <saiadithya2095@gmail.com>
2025-12-01 17:28:07 +0530

    finalQuery.sql

--- finalQuery.sql
+++ finalQuery.sql
@@ -1,6 +1,12 @@
-    SELECT MAX(salary) AS second_highest_salary
-    FROM employees
-    WHERE salary < (SELECT MAX(salary) FROM employees);
-
-
-
+"SELECT d.DEPARTMENT_NAME,\n" +
+               "  AVG(TIMESTAMPDIFF(YEAR, e.DOB, CURDATE())) AS AVERAGE_AGE,\n" +
+               "  TRIM(BOTH ', ' FROM SUBSTRING_INDEX(\n" +
+               "    GROUP_CONCAT(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) ORDER BY e.EMP_ID SEPARATOR ', '),\n" +
+               "    ',', 10\n" +
+               "  )) AS EMPLOYEE_LIST\n" +
+               "FROM DEPARTMENT d\n" +
+               "JOIN EMPLOYEE e ON d.DEPARTMENT_ID = e.DEPARTMENT\n" +
+               "JOIN PAYMENTS p ON e.EMP_ID = p.EMP_ID\n" +
+               "WHERE p.AMOUNT > 70000\n" +
+               "GROUP BY d.DEPARTMENT_ID, d.DEPARTMENT_NAME\n" +
+               "ORDER BY d.DEPARTMENT_ID DESC";
